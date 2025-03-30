7cf859151f712380db7ebff309e104cd9b5ef3ef
zarkonow <stojanov1987@gmail.com>
2025-03-30 15:37:41 +0200

    add user component with styles and integrate into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent],
 })
 export class AppComponent {
   title = 'first-angular-app';
