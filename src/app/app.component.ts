f321d3afaa204609be7608b3e78c0e9b7409d8ff
zarkonow <stojanov1987@gmail.com>
2025-03-30 15:49:08 +0200

    refactor app component: update template structure and remove header import; add dummy user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 
 @Component({
@@ -7,7 +6,7 @@ import { UserComponent } from "./user/user.component";
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent],
+  imports: [ UserComponent],
 })
 export class AppComponent {
   title = 'first-angular-app';
