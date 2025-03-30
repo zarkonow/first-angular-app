bca3c15bb305446566d475a84cb77b60493fc56f
zarkonow <stojanov1987@gmail.com>
2025-03-30 13:47:10 +0200

    add logo to header component and apply styles

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
     selector: 'app-header',
     standalone: true,
     templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
     
 
 })
