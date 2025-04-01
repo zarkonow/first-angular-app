769008a9191d1007769def232f57cf3f66a2b31c
zarkonow <stojanov1987@gmail.com>
2025-04-01 17:58:55 +0200

    update app component: refactor template syntax for user list and task display, removing unnecessary comments and imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,26 +2,25 @@ import { Component } from '@angular/core';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from "./tasks/tasks.component";
-import { NgFor } from '@angular/common';
-import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [UserComponent, TasksComponent],
 })
 export class AppComponent {
   users = DUMMY_USERS;
   selectedUserId?: string;
 
-  get selectedUser(){
+  get selectedUser() {
     return this.users.find((users) => users.id === this.selectedUserId)!;
   }
   onSelectUser(id: string) {
-    
-this.selectedUserId = id;
+
+    this.selectedUserId = id;
 
   }
 
