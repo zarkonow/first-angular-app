3a3d9ab9f3d61335e432a0e2918489395e8e471a
zarkonow <stojanov1987@gmail.com>
2025-04-01 21:18:45 +0200

    update tasks component: add dummy tasks data and initialize task object in tasks component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -9,4 +9,11 @@ import { TaskComponent } from "./task/task.component";
 })
 export class TasksComponent {
 @Input() name?:string;
+ tasks = {
+  id: 't1',
+  userId: 'u1',
+  title: 'Master Angular',
+  summary: 'Learn Angular from scratch and more advanced topics',
+  sueData: '2025-12-31'
+ }
 }
