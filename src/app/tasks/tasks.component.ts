0c6b4145d1e41d59d887fd3e95416f43c41143c0
zarkonow <stojanov1987@gmail.com>
2025-04-01 18:49:08 +0200

    update tasks component: implement task display with new layout and styles, add task component for individual task rendering

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
