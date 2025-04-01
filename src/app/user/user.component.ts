b4c8d5c0ad5e05e05841d8354eba40d74de58e5c
zarkonow <stojanov1987@gmail.com>
2025-04-01 16:47:43 +0200

    update user component: refactor to use user object for properties and simplify image handling

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,9 +12,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
+  
   @Output() select = new EventEmitter();
   //select = output<string>()
 
@@ -27,11 +30,11 @@ export class UserComponent {
   // })
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id)
+    this.select.emit(this.user.id)
   }
 
 }
