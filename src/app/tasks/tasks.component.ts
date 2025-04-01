fc73521f44e6d557c50c0656ef68f64fe8161c3e
zarkonow <stojanov1987@gmail.com>
2025-04-01 22:22:57 +0200

    update tasks component: enhance task data with additional entries and summaries, improve task rendering; update global styles for better UI

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -10,20 +10,31 @@ import { TaskComponent } from "./task/task.component";
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
-  tasks = [{
-    id: 't1',
-    userId: 'u1',
-    title: 'Master Angular',
-    summary: 'Learn Angular from scratch and more advanced topics',
-    dueDate: '2025-12-31',
-  },
-  {
-    id: 't1',
-    userId: 'u1',
-    title: 'Master Angular',
-    summary: 'Learn Angular from scratch and more advanced topics',
-    dueDate: '2025-12-31'
-  },
+
+  tasks =  [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
   ]
 
   get selectedUserTasks() {
