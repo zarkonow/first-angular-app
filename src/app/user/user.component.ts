5f771162beccffaf7cacc1b337b33c20ac8ee87e
zarkonow <stojanov1987@gmail.com>
2025-04-01 13:11:27 +0200

    refactor user component: update output property to use signal for user selection

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, EventEmitter, Input, input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, output, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 
@@ -15,8 +15,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
-
+  //@Output() select = new EventEmitter();
+  select = output<string>()
 
 
   // avatar = input.required<string>()
