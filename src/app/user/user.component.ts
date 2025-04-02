c1d85b928026b484682538f1d908e910104e7670
zarkonow <stojanov1987@gmail.com>
2025-04-02 17:13:01 +0200

    update user component: add selected input to highlight active user, enhance button styling for better user experience

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,17 +11,9 @@ import { User } from './user.model';
 
 export class UserComponent {
   @Input({ required: true }) user!: User;
-
+  @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter();
-  //select = output<string>()
 
-
-  // avatar = input.required<string>()
-  // name = input.required<string>()
-
-  // imagePath = computed(() => {
-  //   return 'assets/users/' + this.avatar;
-  // })
 
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
