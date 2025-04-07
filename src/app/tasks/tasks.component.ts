350d5ca93e35c6613f78c7d04ce4e3dae539b2a2
zarkonow <stojanov1987@gmail.com>
2025-04-07 15:19:18 +0200

    update new-task and tasks components: implement task submission with data binding; enhance task creation functionality

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -56,6 +57,18 @@ export class TasksComponent {
 
 onCancleAddTask() {
   this.isAddingTask = false
+}
+
+onAddTask(taskData: NewTaskData) {
+this.tasks.unshift({
+  id: new Date().getTime().toString(),
+  title: taskData.title,
+  summary: taskData.summary,
+  dueDate: taskData.date,
+  userId: this.userId,
+})
+this.isAddingTask = false
+
 }
 
 }
