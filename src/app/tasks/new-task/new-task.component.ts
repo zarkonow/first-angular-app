958ed43aca6e3318344638deb6c8e97cfe3c6f1f
zarkonow <stojanov1987@gmail.com>
2025-04-07 14:37:35 +0200

    update new-task component: bind title input to model and fix cancel button handler; integrate cancel event in tasks component

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,15 +1,16 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-
   @Output() cancel = new EventEmitter<void>();
-  onCancel(){
-    this.
+  enteredTitle = ''
+  onCancel() {
+    this.cancel.emit()
   }
 }
